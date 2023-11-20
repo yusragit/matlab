07542f3b159948d47a43330f9a838647a7a49ec5
yusra saeed <148749353+yusragit@users.noreply.github.com>
2023-11-20 13:46:18 +0500

    Add files via upload

---
+++ lab05sns.m
@@ -0,0 +1,14 @@
+t=-1:0.005:0.995;
+xt=cos(2*pi*t/2);
+plot(t,xt)
+absxt=abs(xt.^2)
+absxt=abs(xt).^2
+deltat=0.005;
+T=2;
+pxt=sum(absxt)*deltat/T
+n=0:19;
+xn=cos(2*pi*n/20)
+stem(n,xn)
+N=20;
+absxn=abs(xn.^2);
+pxn=sum(absxn)/N
