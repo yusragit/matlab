f8d52b10b098d523c205bdc36ba1111e0632723d
yusra saeed <148749353+yusragit@users.noreply.github.com>
2023-11-20 16:18:57 +0500

    Add files via upload

---
+++ lab 10sns.m
@@ -0,0 +1,22 @@
+1/(1+w2)ˆ
+» f=ifourier(F)
+f =
+1/2*exp(-x)*heaviside(x)+1/2*exp(x)*heaviside(-x)
+» subplot(211)
+» ezplot(F)
+» subplot(212)
+» ezplot(f)1/(1+w2)ˆ
+» f=ifourier(F)
+f =
+1/2*exp(-x)*heaviside(x)+1/2*exp(x)*heaviside(-x)
+» subplot(211)
+» ezplot(F)
+» subplot(212)
+» ezplot(f)1/(1+w2)ˆ
+» f=ifourier(F)
+f =
+1/2*exp(-x)*heaviside(x)+1/2*exp(x)*heaviside(-x)
+» subplot(211)
+» ezplot(F)
+» subplot(212)
+» ezplot(f)
